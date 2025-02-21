6d16ed3c8c14714cadb66e21dd2d845f3859c05d
dhanuvagman006 <dhnaushinsit@gmail.com>
2025-02-21 18:00:12 +0530

    created files

---
+++ main.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+
+def listen():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Please Start Speaking...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio_data = recognizer.listen(source)
+        try:
+            text = recognizer.recognize_google(audio_data)
+            print(text)
+        except:
+            print("WE GOT SOME ERROR")
+            exit()
+        if "exit" in text:
+            exit()
+        return text
+listen()
